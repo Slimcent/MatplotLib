291109cdb43f3def58ad015b7afb503507c07516
Slimcent <centpresley@gmail.com>
2023-08-13 20:08:07 +0100

    Bar graph

--- main.py
+++ main.py
@@ -82,15 +82,101 @@ import numpy as np
 #
 # plt.show()
 
-x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
-y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+# x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+# y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+#
+# font1 = {'family': 'serif', 'color': 'blue', 'size': 20}  # Setting the font size
+# font2 = {'family': 'serif', 'color': 'darkred', 'size': 15}
+#
+# plt.title("Sports Watch Data", fontdict=font1)
+# plt.xlabel("Average Pulse", fontdict=font2)
+# plt.ylabel("Calorie Burnage", fontdict=font2)
+#
+# plt.plot(x, y)
+# plt.show()
 
-font1 = {'family': 'serif', 'color': 'blue', 'size': 20}  # Setting the font size
-font2 = {'family': 'serif', 'color': 'darkred', 'size': 15}
+# Adding grid
+# x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+# y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+#
+# plt.title("Sports Watch Data")
+# plt.xlabel("Average Pulse")
+# plt.ylabel("Calorie Burnage")
+#
+# plt.plot(x, y)
+#
+# plt.grid()
+# plt.show()
 
-plt.title("Sports Watch Data", fontdict=font1)
-plt.xlabel("Average Pulse", fontdict=font2)
-plt.ylabel("Calorie Burnage", fontdict=font2)
+# x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+# y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+#
+# plt.title("Sports Watch Data")
+# plt.xlabel("Average Pulse")
+# plt.ylabel("Calorie Burnage")
+#
+# plt.plot(x, y)
+#
+# plt.grid(axis='x')  # specify to show x(vertical) grid lines
+# plt.show()
 
-plt.plot(x, y)
+# x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+# y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+#
+# plt.title("Sports Watch Data")
+# plt.xlabel("Average Pulse")
+# plt.ylabel("Calorie Burnage")
+#
+# plt.plot(x, y)
+#
+# plt.grid(axis='y')  # specify to show x(vertical) grid lines
+# plt.show()
+
+# Display multiple plots
+# plot 1:
+# x = np.array([0, 1, 2, 3])
+# y = np.array([3, 8, 1, 10])
+#
+# plt.subplot(1, 2, 1)
+# plt.plot(x, y)
+# plt.title("SALES")  # plot one title
+#
+# # plot 2:
+# x = np.array([0, 1, 2, 3])
+# y = np.array([10, 20, 30, 40])
+#
+# plt.subplot(1, 2, 2)
+# plt.plot(x, y)
+# plt.title("INCOME")  # plot two title
+#
+# plt.suptitle("MY SHOP")  # plot overall title
+# plt.show()
+
+# Draw two plots on top of each other
+# plot 1:
+# x = np.array([0, 1, 2, 3])
+# y = np.array([3, 8, 1, 10])
+#
+# plt.subplot(2, 1, 1)
+# plt.plot(x, y)
+#
+# # plot 2:
+# x = np.array([0, 1, 2, 3])
+# y = np.array([10, 20, 30, 40])
+#
+# plt.subplot(2, 1, 2)
+# plt.plot(x, y)
+#
+# plt.show()
+
+# Using Bar graph
+# x = np.array(["A", "B", "C", "D"])
+# y = np.array([3, 8, 1, 10])
+#
+# plt.bar(x, y, color="red")
+# plt.show()
+
+x = np.array(["A", "B", "C", "D"])
+y = np.array([3, 8, 1, 10])
+plt.barh(x, y, color="hotpink")  # Horizontal bars
 plt.show()
